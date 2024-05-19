9a874462ef214cb2acf7ed52b9c7700e2d7a0741
Mayurchaudhari0795 <mayurchaudhari9000@gmail.com>
2024-05-19 15:29:11 +0530

    completed data type in javascript

---
+++ Datatypes/index.js
@@ -0,0 +1,29 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// . 1. What are the different data types in JavaScript? List them and provide an  example of each ?
+
+// ans - this fsdasjdflajsdfq
+
+
+
+
+
+
+
+
+
